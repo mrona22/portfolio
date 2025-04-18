acb6c8942cfec917530b79752a2c58897921089c
milanrona <mrona@ucsd.edu>
2025-04-18 12:48:34 -0700

    js, works correctly
    correctly

--- global.js
+++ global.js
@@ -1,5 +1,25 @@
 console.log('IT’S ALIVE!');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url : 'contact/', title: 'Contact' },
+  ];
+
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
