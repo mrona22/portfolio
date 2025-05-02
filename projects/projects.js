412dc48f31b83b65f8234d4014791e1d3b5c09c9
milanrona <mrona@ucsd.edu>
2025-05-02 09:39:51 -0700

    added project year by

--- projects/projects.js
+++ projects/projects.js
@@ -6,54 +6,88 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-  });
-
-  let rolledData = d3.rollups(
-    projects,
-    (v) => v.length,
-    (d) => d.year,
-  );
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
+
+
+
+
+function renderPieChart(projectsGiven) {
+    // re-calculate rolled data
+    let newRolledData = d3.rollups(
+      projectsGiven,
+      (v) => v.length,
+      (d) => d.year,
+    );
+    // re-calculate data
+    let newData = newRolledData.map(([year, count]) => {
+      return { value: count, label: year };
+    });
+    // re-calculate slice generator, arc data, arc, etc.
+    let newSliceGenerator = d3.pie().value((d) => d.value);
+    let newArcData = newSliceGenerator(newData);
+    let newArcs = newArcData.map((d) => arcGenerator(d));
+    // TODO: clear up paths and legends
+    let legend = d3.select('.legend')
+    legend.selectAll('*').remove();
+    let newSVG = d3.select('svg');
+    newSVG.selectAll('path').remove();
+
+    newArcs.forEach((arc, idx) => {
+        // TODO, fill in step for appending path to svg using D3
+        newSVG.append('path').attr('d', arc).attr('fill', colors(idx));
+    });
+    // update paths and legends, refer to steps 1.4 and 2.2
+    newData.forEach((d, idx) => {
+        legend
+          .append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .attr('class', 'legend-item')
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+    });
+  }
+  
+  // Call this function on page load
+  renderPieChart(projects);
+  
+  let query = '';
+  let searchInput = document.querySelector('.searchBar');
+  searchInput.addEventListener('input', (event) => {
+    query = event.target.value.toLowerCase();
+    
+    // Filter projects based on query
+    let filteredProjects = projects.filter((project) => {
+      let values = Object.values(project).join('\n').toLowerCase();
+      return values.includes(query);
+    });
+    
+    // Re-render the projects and pie chart when the search query changes
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+    renderPieChart(filteredProjects);
   });
 
+//   let selectedIndex = -1;
+//   let svg = d3.select('svg');
+  
+//   // Clear previous paths
+//   svg.selectAll('path').remove();
 
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-
-arcs.forEach((arc, idx) => {
-    // TODO, fill in step for appending path to svg using D3
-    d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
-  });
-
-let legend = d3.select('.legend');
-
-data.forEach((d, idx) => {
-  legend
-    .append('li')
-    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-    .attr('class', 'legend-item')
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-});
-
-let query = '';
-let searchInput = document.querySelector('.searchBar');
-searchInput.addEventListener('change', (event) => {
-  // update query value
-  query = event.target.value;
-  // filter projects
-  let filteredProjects = projects.filter((project) => {
-    let values = Object.values(project).join('\n').toLowerCase();
-    return values.includes(query.toLowerCase());
-  });
-  // render filtered projects
-  renderProjects(filteredProjects, projectsContainer, 'h2');
-});
-
-
+//   let arcs = arcData.map((d) => arcGenerator(d));
+//   arcs.forEach((arc, i) => {
+//     svg
+//       .append('path')
+//       .attr('d', arc)
+//       .attr('fill', colors(i))
+//       .on('click', function() {
+//         // Toggle the selected index
+//         selectedIndex = selectedIndex === i ? -1 : i;
+  
+//         // Update paths by toggling the 'selected' class
+//         svg.selectAll('path')
+//           .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+  
+//         // Update legend items by toggling the 'selected' class
+//         legend.selectAll('li')
+//           .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+//       });
+//   });
